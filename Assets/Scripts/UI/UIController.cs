ede338fc8d04528d519fd5daa0f5933cd6c21d04
goon <nathan.gooneratne@gmail.com>
2023-05-20 18:36:24 -0400

    added leantween + finished UI layout
    tentative button text/design but it's ready for programming at least

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject credits;
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject building;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
