f5776967a4d28cf5c03d4f4e48bc245a71d1be45
goon <nathan.gooneratne@gmail.com>
2023-05-21 01:30:55 -0400

    ui swapping, button hooks, tweens

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class UIController : MonoBehaviour
 {
@@ -7,14 +9,110 @@ public class UIController : MonoBehaviour
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject building;
 
+    public enum UIState
+    {
+        None,
+        MainMenu,
+        Credits,
+        PauseMenu,
+        Building
+    }
+
+    private UIState _currentState;
+    private UIState _priorState;
+
+    // defo a better way to do this than to have two dicts but whatever
+    private Dictionary<UIState, GameObject> _objectMap;
+
+    // maps UIStates to all the children of the UI they belong to
+    private Dictionary<UIState, RectTransform[]> _childMap;
+
+    private void Awake()
+    {
+        // set up the child dictionary
+        _childMap = new Dictionary<UIState, RectTransform[]>();
+        _childMap.Add(UIState.MainMenu, mainMenu.GetComponentsInChildren<RectTransform>());
+        _childMap.Add(UIState.Credits, credits.GetComponentsInChildren<RectTransform>());
+        _childMap.Add(UIState.PauseMenu, pauseMenu.GetComponentsInChildren<RectTransform>());
+        _childMap.Add(UIState.Building, building.GetComponentsInChildren<RectTransform>());
+
+        UIState[] allButMain = new UIState[]
+        {
+            UIState.Credits, UIState.PauseMenu, UIState.Building
+        };
+
+        // set the scale of everything to zero.
+        foreach (UIState state in allButMain)
+        {
+            RectTransform[] forms = _childMap[state];
+
+            foreach (RectTransform rt in forms)
+            {
+                rt.localScale = Vector3.zero;
+            }
+        }
+
+        // set up the parent dictionary
+        _objectMap = new Dictionary<UIState, GameObject>();
+
+        _objectMap.Add(UIState.MainMenu, mainMenu);
+        _objectMap.Add(UIState.Credits, credits);
+        _objectMap.Add(UIState.PauseMenu, pauseMenu);
+        _objectMap.Add(UIState.Building, building);
+
+        _currentState = UIState.MainMenu;
+        _priorState = UIState.MainMenu;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // animate into the main menu
+        //Invoke(nameof(test), 2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeState(UIState state)
     {
-        
+        // this works because by the time the onComplete method is invoked, the priorState field will
+        // have been reassigned, meaning that we disable the right thing.
+        StartTweenOnAll(_childMap[_currentState], Vector3.zero, 0.4f,
+            () => { ToggleParent(_objectMap[_priorState], false); }
+        );
+
+        if (state != UIState.None)
+        {
+            ToggleParent(_objectMap[state], true);
+
+            StartTweenOnAll(_childMap[state], Vector3.one, 0.5f);
+        }
+
+        _priorState = _currentState;
+        _currentState = state;
+    }
+
+    void ToggleParent(GameObject parent, bool state)
+    {
+        parent.SetActive(state);
+    }
+
+    int[] StartTweenOnAll(RectTransform[] objects, Vector3 tweenDest, float tweenTime, Action action = null)
+    {
+        int[] tweenIds = new int[objects.Length];
+
+        int index = 0;
+
+        float tweenTimeOffset = 0.1f;
+
+        foreach (RectTransform rt in objects)
+        {
+            tweenIds[index] = 
+                LeanTween.scale(rt, tweenDest, tweenTime + tweenTimeOffset * index)
+                .setOnComplete(action)
+                .setEaseInOutBack()
+                .id;
+
+            index += 1;
+        }
+        return tweenIds;
     }
 }
---
+++ Assets/Scripts/UI/UIMethodWrapper.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+// A wrapper for exposing methods for button UnityEvents.
+// i thought it would look nicer than having everything in UIController :)
+public class UIMethodWrapper : MonoBehaviour
+{
+    UIController controller;
+    CameraPanControl panControl;
+
+    private void Awake()
+    {
+        controller = GetComponent<UIController>();
+        panControl = GetComponent<CameraPanControl>();
+    }
+
+    public void GotoMain()
+    {
+        controller.ChangeState(UIController.UIState.MainMenu);
+    }
+
+    public void GotoPause()
+    {
+        controller.ChangeState(UIController.UIState.PauseMenu);
+    }
+
+    public void GotoCredits()
+    {
+        controller.ChangeState(UIController.UIState.Credits);
+    }
+
+    public void GotoBuild()
+    {
+        controller.ChangeState(UIController.UIState.Building);
+    }
+
+    public void GotoNone()
+    {
+        controller.ChangeState(UIController.UIState.None);
+    }
+
+    public void GotoGame()
+    {
+        controller.ChangeState(UIController.UIState.None);
+        panControl.AnimateCameraPosition(CameraPanControl.CameraPositions.Destination);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
